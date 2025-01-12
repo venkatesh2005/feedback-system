7af22f8e8f347fa8dcea0a8e3f5430e1b76865ce
VENKATESH M <115524761+venkatesh2005@users.noreply.github.com>
2025-01-12 14:21:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session, send_file
 from pymongo import MongoClient
 import uuid
 from flask import Flask, render_template, request, redirect, url_for, flash, send_file
